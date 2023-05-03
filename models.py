f5c02e8213e2c129957b8670fb33b18ae47ca96b
Vaughn Seekamp <vseekamp@gmail.com>
2023-05-03 10:27:19 -0700

    began work on models

--- models.py
+++ models.py
@@ -1,13 +1,31 @@
 """SQLAlchemy models for Friendly."""
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 DEFAULT_IMG = "https://www.shutterstock.com/image-vector/clown-emoji-face-vector-600w-1306928644.jpg"
 
 
-from flask_bcrypt import Bcrypt
-from flask_sqlalchemy import SQLAlchemy
-
 bcrypt = Bcrypt()
 db = SQLAlchemy()
+
+# TODO: match! or maybe like
+class Like(db.Model):
+    """Connection of a liker <-> liked user."""
+
+    __tablename__ = 'likes'
+
+    user_being_liked = db.Column(
+        db.Text,
+        db.ForeignKey('users.username', ondelete="cascade"),
+        primary_key=True,
+    )
+
+    user_liking = db.Column(
+        db.Text,
+        db.ForeignKey('users.username', ondelete="cascade"),
+        primary_key=True,
+    )
+
 
 class User(db.Model):
     """User in the system."""
@@ -47,7 +65,7 @@ class User(db.Model):
         nullable=False,
     )
 
-    radius = db.Column(
+    friend_radius = db.Column(
         db.Integer,
         nullable=False,
     )
@@ -65,19 +83,91 @@ class User(db.Model):
         backref="matching",
     )
 
-class Matches(db.Model):
-    """Connection of a follower <-> followed_user."""
+    @classmethod
+    def signup( username, email, password, hobbies, interests, zip, friend_radius, image=DEFAULT_IMG):
+        """Sign up user.
 
-    __tablename__ = 'matches'
+        Hashes password and adds user to system.
+        """
 
-    user_being_liked = db.Column(
-        db.Text,
-        db.ForeignKey('users.username', ondelete="cascade"),
-        primary_key=True,
-    )
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
 
-    user_liking = db.Column(
-        db.Text,
-        db.ForeignKey('users.username', ondelete="cascade"),
-        primary_key=True,
-    )
+        user = User(
+            username=username,
+            email=email,
+            password=hashed_pwd,
+            image=image,
+            hobbies=hobbies,
+            interests=interests,
+            zip=zip,
+            friend_radius=friend_radius,
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If this can't find matching user (or if password is wrong), returns
+        False.
+        """
+
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+
+    # def is_followed_by(self, other_user):
+    #     """Is this user followed by `other_user`?"""
+
+    #     found_user_list = [
+    #         user for user in self.followers if user == other_user]
+    #     return len(found_user_list) == 1
+
+    # def is_following(self, other_user):
+    #     """Is this user following `other_use`?"""
+
+    #     found_user_list = [
+    #         user for user in self.following if user == other_user]
+    #     return len(found_user_list) == 1
+
+
+# class Message(db.Model):
+#     """An individual message ("warble").
+
+#     Message -> who liked it, backref="liked_by"
+#     """
+
+#     __tablename__ = 'messages'
+
+#     id = db.Column(
+#         db.Integer,
+#         primary_key=True,
+#     )
+
+#     text = db.Column(
+#         db.String(140),
+#         nullable=False,
+#     )
+
+#     timestamp = db.Column(
+#         db.DateTime,
+#         nullable=False,
+#         default=datetime.utcnow,
+#     )
+
+#     user_id = db.Column(
+#         db.Integer,
+#         db.ForeignKey('users.id', ondelete='CASCADE'),
+#         nullable=False,
+#     )
